b08e550d44fa4e845cfaef22318ef740fd548ec4
hota1024 <hotalog@hota1024.com>
2021-06-30 20:06:16 +0900

    🏷️ add validateUser method to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,26 @@
+import bcrypt from 'bcrypt'
 import { Injectable } from '@nestjs/common'
-import { UsersService } from 'src/users/users.service'
+import { User } from 'src/users/user.entity'
+import { UserPublic, UsersService } from 'src/users/users.service'
 
 @Injectable()
 export class AuthService {
   private readonly usersService: UsersService
+
+  /**
+   * validate credentials and returns the user public.
+   *
+   * @param email email.
+   * @param password password.
+   */
+  async validateUser(
+    email: User['email'],
+    password: string
+  ): Promise<UserPublic | void> {
+    const user = await this.usersService.findByEmail(email)
+
+    if (user && bcrypt.compareSync(password, user.passwordHash)) {
+      return this.usersService.toPublic(user)
+    }
+  }
 }
