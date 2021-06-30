559463bca55e372ff890479c75674bacfd1fdc53
hota1024 <hotalog@hota1024.com>
2021-06-30 23:46:44 +0900

    🏷️ add auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from 'src/users/users.module'
 import { AuthService } from './auth.service'
 import { JwtStrategy } from './jwt.strategy'
 import { LocalStrategy } from './local.strategy'
+import { AuthController } from './auth.controller'
 
 @Module({
   imports: [
@@ -18,5 +19,6 @@ import { LocalStrategy } from './local.strategy'
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
