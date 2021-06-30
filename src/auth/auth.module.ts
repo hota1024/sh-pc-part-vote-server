b2bfd4bda8fb3cefca8efc93b9a69ce11db9e4db
hota1024 <hotalog@hota1024.com>
2021-06-30 23:40:13 +0900

    🏷️ add starategies and modules imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { UsersModule } from 'src/users/users.module'
 import { AuthService } from './auth.service'
+import { JwtStrategy } from './jwt.strategy'
+import { LocalStrategy } from './local.strategy'
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
