20b171b42f59f7837b2f1ca21194f04b8d04b753
hota1024 <hotalog@hota1024.com>
2021-07-01 00:03:09 +0900

    🏷️ add login method to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,13 @@ import bcrypt from 'bcrypt'
 import { Injectable } from '@nestjs/common'
 import { User } from 'src/users/user.entity'
 import { UserPublic, UsersService } from 'src/users/users.service'
+import { JwtService } from '@nestjs/jwt'
+import { LoginDto } from './auth.dto'
 
 @Injectable()
 export class AuthService {
   private readonly usersService: UsersService
+  private readonly jwtService: JwtService
 
   /**
    * validate credentials and returns the user public.
@@ -22,5 +25,14 @@ export class AuthService {
     if (user && bcrypt.compareSync(password, user.passwordHash)) {
       return this.usersService.toPublic(user)
     }
+  }
+
+  /**
+   * sign user data by jwt.
+   *
+   * @param credentials credentials.
+   */
+  login(credentials: LoginDto): string {
+    return this.jwtService.sign(credentials)
   }
 }
