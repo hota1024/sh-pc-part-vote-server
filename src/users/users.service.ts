3a8a46d25d209358a06dc53828b48e7e892a6977
hota1024 <hotalog@hota1024.com>
2021-06-30 18:13:22 +0900

    🏷️ add findById method to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common'
+import { Repository } from 'typeorm'
+import { User } from './user.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly usersRepo: Repository<User>
+
+  /**
+   * find a user by id.
+   *
+   * @param id user id.
+   */
+  findById(id: User['id']): Promise<User> {
+    return this.usersRepo.findOne(id)
+  }
+}
