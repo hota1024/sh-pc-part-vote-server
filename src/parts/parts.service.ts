1bd7ee83d8721411392a2b5d3a90a1a80fc95aa6
hota1024 <hotalog@hota1024.com>
2021-07-01 22:33:50 +0900

    🏷️ add listPublicParts method to parts service

--- src/parts/parts.service.ts
+++ src/parts/parts.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Part, PartType } from './part.entity'
+
+/**
+ * PartPublic type.
+ */
+export type PartPublic = {
+  name: string
+  type: PartType
+  votes: number
+}
 
 @Injectable()
-export class PartsService {}
+export class PartsService {
+  @InjectRepository(Part)
+  private readonly partsRepo: Repository<Part>
+
+  /**
+   * returns all public parts.
+   */
+  async listPublicParts(): Promise<PartPublic[]> {
+    const parts = await this.partsRepo.find({ relations: ['users'] })
+
+    return parts.map(this.toPublic)
+  }
+
+  /**
+   * returns part public.
+   *
+   * @param part part.
+   */
+  toPublic(part: Part): PartPublic {
+    return {
+      name: part.name,
+      type: part.type,
+      votes: part.users?.length ?? 0,
+    }
+  }
+}
