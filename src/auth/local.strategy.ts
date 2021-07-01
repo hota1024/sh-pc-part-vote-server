c21994240fd7116045ba6f7f85daf70b149d86e0
hota1024 <hotalog@hota1024.com>
2021-07-01 18:06:46 +0900

    🐛 add injectable to LocalStrategy

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { UnauthorizedException } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-local'
 import { User } from 'src/users/user.entity'
@@ -8,20 +8,21 @@ import { AuthService } from './auth.service'
 /**
  * LocalStrategy class.
  */
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  private authService: AuthService
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'email',
+    })
+  }
 
   async validate(email: User['email'], password: string): Promise<UserPublic> {
-    try {
-      const user = await this.authService.validateUser(email, password)
+    const user = await this.authService.validateUser(email, password)
 
-      if (!user) {
-        throw new UnauthorizedException()
-      }
-
-      return user
-    } catch {
+    if (!user) {
       throw new UnauthorizedException()
     }
+
+    return user
   }
 }
