8c213b8bc60e7049a310263930b9abc97852c4c2
hota1024 <hotalog@hota1024.com>
2021-07-01 21:48:38 +0900

    🏷️ add parts service

--- src/parts/parts.module.ts
+++ src/parts/parts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Part } from './part.entity'
+import { PartsService } from './parts.service'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Part])],
+  providers: [PartsService],
 })
 export class PartsModule {}
