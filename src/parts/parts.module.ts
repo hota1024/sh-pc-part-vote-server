b86ae0a186058a511716cfa4805d12df854b3bf8
hota1024 <hotalog@hota1024.com>
2021-07-02 07:18:44 +0900

    🏷️ add User model import to parts module

--- src/parts/parts.module.ts
+++ src/parts/parts.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Part } from './part.entity'
 import { PartsService } from './parts.service'
 import { PartsController } from './parts.controller'
+import { User } from 'src/users/user.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Part])],
+  imports: [TypeOrmModule.forFeature([Part, User])],
   providers: [PartsService],
   controllers: [PartsController],
 })
